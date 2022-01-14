cb548c757dde7fb52fc3611fe65d51884020fd14
Timo <timodb031@gmail.com>
2022-01-14 12:06:44 +0200

    contact page contact info card and map card defined

--- src/contactPage.js
+++ src/contactPage.js
@@ -13,7 +13,36 @@ function generateContactContainer () {
     const pageContent = document.getElementById('pageContent');
 
     pageContent.appendChild(contactContainer);
+    pageContent.appendChild(generateContactInfoCard());
+    pageContent.appendChild(generateMapCard());
 
     return contactContainer
 
+}
+
+function generateContactInfoCard(){
+    const contactInfoCard = document.createElement('div');
+    contactInfoCard.id = 'contact-info-card';
+    contactInfoCard.innerHTML = `this is the contact info card`;
+
+    const contactInfoTitle = document.createElement('h4');
+    contactInfoTitle.id = 'contact-info-title';
+    contactInfoTitle.innerText = `this is the contact info title`;
+
+    const contactInfoText = document.createElement('p');
+    contactInfoText.id = 'contact-info-text';
+    contactInfoText.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos iure corporis sint quae recusandae repudiandae cum eius dolores.`;
+
+    contactInfoCard.appendChild(contactInfoTitle);
+    contactInfoCard.appendChild(contactInfoText);
+    
+    return contactInfoCard
+}
+
+function generateMapCard(){
+    const mapCard = document.createElement('div');
+    mapCard.id = 'map-card';
+    mapCard.innerHTML = `this is the map card`;
+
+    return mapCard
 }
